360b5c12da3799449c8b8557808f490645270af6
Victor <victor.badenas@gmail.com>
2020-09-04 10:15:39 +0200

    added basic train structure

--- src/trainer.py
+++ src/trainer.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import torch
 import logging
 from model import Model
@@ -10,19 +8,36 @@ class Trainer():
     def __init__(self, parameters):
         self.parameters = parameters
         self.init_model()
+        self.init_torch_modules()
 
     def init_model(self):
         self.model = Model(self.parameters)
         logging.info(self.model)
         self.modelIO = ModelIO(self.parameters, self.model)
-        self.epoch = 0
+        self.start_epoch = 0
         if self.parameters.checkpoint is not None:
             logging.info(f"loading checkpoint from path {self.parameters.checkpoint}")
-            self.epoch = self.modelIO.load_model_checkpoint(self.parameters.checkpoint)
-            logging.info(f"checkpoint loaded in epoch {self.epoch}")
+            self.start_epoch = self.modelIO.load_model_checkpoint(self.parameters.checkpoint)
+            logging.info(f"checkpoint loaded in epoch {self.start_epoch}")
+
+    def init_torch_modules(self):
+        self.optimizer = None
 
     def __call__(self, *args, **kwargs):
         self.train_model(*args, **kwargs)
 
     def train_model(self):
-        pass
+        logging.info(f"Stating training from epoch {self.start_epoch}")
+        for epochIdx in range(self.start_epoch, self.parameters.epochs):
+            logging.info(f"Epoch: {epochIdx}")
+            logging.info("Train stage")
+            train_accuracy = self.train_epoch()
+            logging.info("Test stage")
+            test_accuracy = self.test_epoch()
+            self.modelIO.save_model(epochIdx, test_accuracy)
+
+    def train_epoch(self):
+        return 0.0
+
+    def test_epoch(self):
+        return 0.0
