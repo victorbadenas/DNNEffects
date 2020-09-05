3a0f028e6b37469fd93e9f5c474284828408a703
Victor <victor.badenas@gmail.com>
2020-09-05 16:41:32 +0200

    moved to root path

--- scripts/build_lsts.sh
+++
@@ -1,3 +0,0 @@
-for $effect in $(echo Chorus Distortion EQ FeedbackDelay Flanger Overdrive Phaser Reverb SlapbackDelay Tremolo Vibrato); do
-    python createLstDataset.py -i E:/IDMT-SMT-AUDIO-EFFECTS/ -e $effect
-done;
