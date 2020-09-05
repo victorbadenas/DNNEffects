15a43d13ee778b6a4f81b17568acd2855586dd41
Victor <victor.badenas@gmail.com>
2020-09-05 16:42:05 +0200

    added basic test model

--- src/model.py
+++ src/model.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 import torch.functional as F
 
 class Model(nn.Module):
-    def __init__(self, parameters):
+    def __init__(self, training_parameters):
         super(Model, self).__init__()
-        self.parameters = parameters
+        self.training_parameters = training_parameters
+        self.fc_test = nn.Linear(128, 128)
 
     def forward(self, input_tensor):
-        return input_tensor
+        return self.fc_test(input_tensor)
