efe7ef5fea5ec24027b07ca28bb985d2193d1611
Victor <victor.badenas@gmail.com>
2020-09-06 21:56:36 +0200

    updated lst creation scripts

--- build_lsts.ps1
+++ build_lsts.ps1
@@ -1,4 +1,5 @@
 $array = "Chorus","Distortion","EQ","FeedbackDelay","Flanger","Overdrive","Phaser","Reverb","SlapbackDelay","Tremolo","Vibrato"
 $array | ForEach-Object {} {
-    python createLstDataset.py -i E:/IDMT-SMT-AUDIO-EFFECTS/ -e $_
+    python scripts/createLstDataset.py -i E:/IDMT-SMT-AUDIO-EFFECTS/ -e $_;
+    python scripts/splitLstTrainTest.py -i ./dataset/$_.lst -test 0.1
 } {}
