89c4c51d27fb83bcdc152e9807b186bb947a8090
Victor <victor.badenas@gmail.com>
2020-09-16 14:37:43 +0200

    added init and mock tests

---
+++ tests/model_tests.py
@@ -0,0 +1,12 @@
+import sys
+import unittest
+sys.path.append('./src/')
+
+
+class ModelTests(unittest.TestCase):
+    def test_model_outoput_size(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
