c1ff0c1a978d42ebf3d1d151dc499265c865824f
Victor <victor.badenas@gmail.com>
2020-09-24 17:28:41 +0200

    added metric logging

--- src/model_io.py
+++ src/model_io.py
@@ -33,9 +33,11 @@ class ModelIO:
     def save_model(self, epoch, metric):
         if self.best_metric is None:
             logging.info(f"Saving model in epoch {epoch}")
+            logging.info(f"Metric {self.best_metric:.6f} -> {metric:.6f}")
             self.__save(epoch, metric)
         elif metric < self.best_metric:
             logging.info(f"Saving model in epoch {epoch}")
+            logging.info(f"Metric {self.best_metric:.6f} -> {metric:.6f}")
             self.__save(epoch, metric)
 
     def __save(self, epoch, metric):
