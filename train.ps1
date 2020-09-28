ffa0fb08124a8b01ca0991847468fff7f3488475
Victor <victor.badenas@gmail.com>
2020-09-29 00:14:42 +0200

    changed default frame size to 128

--- train.ps1
+++ train.ps1
@@ -6,6 +6,6 @@ $effect="Distortion"
 $trainLst="./dataset/${effect}/train.lst"
 $testLst="./dataset/${effect}/test.lst"
 $batchSize=256
-$frameLength=1024
+$frameLength=128
 $logFile="./experiments/${experiment}/train.log"
 python src/train.py -n ${experiment} --log_file ${logFile} -trainlst ${trainLst} -testlst ${testLst} --batch_size=$batchSize --frame_length=$frameLength
